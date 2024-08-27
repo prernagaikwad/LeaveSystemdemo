8a0b4aa3277326fc2bea764acc5355fb253d07f8
prernagaikwad <32159684+prernagaikwad@users.noreply.github.com>
2024-08-27 11:02:12 +0530

    Update NotificationService.java

--- src/main/java/com/prernagaikwad/springboot/LeaveSystemdemo/Service/NotificationService.java
+++ src/main/java/com/prernagaikwad/springboot/LeaveSystemdemo/Service/NotificationService.java
@@ -6,7 +6,6 @@ import com.prernagaikwad.springboot.LeaveSystemdemo.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
 
