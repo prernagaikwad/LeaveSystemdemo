d32164b2daa7fcc9b778bc55db48eae5c178ab74
prernagaikwad <32159684+prernagaikwad@users.noreply.github.com>
2024-08-27 11:01:53 +0530

    Update LeaveServiceImpl.java

--- src/main/java/com/prernagaikwad/springboot/LeaveSystemdemo/Service/LeaveServiceImpl.java
+++ src/main/java/com/prernagaikwad/springboot/LeaveSystemdemo/Service/LeaveServiceImpl.java
@@ -4,7 +4,6 @@ import com.prernagaikwad.springboot.LeaveSystemdemo.DAO.LeaveRepository;
 import com.prernagaikwad.springboot.LeaveSystemdemo.Entity.Leave;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
